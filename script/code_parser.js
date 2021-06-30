36fee082ce8ef1fa75365368acaa779918e81288
Lumy Kelvin <lunox273@gmail.com>
2021-06-30 13:13:11 +0900

    rename snake form

--- script/code_parser.js
+++ script/code_parser.js
@@ -9,16 +9,16 @@ class CodeParser {
         break;
     }
   }
-  Run(text) {
+  run(text) {
     let code;
     let codeGroup = [];
     while ((code = this.codeReg.exec(text)) !== null) {
       let pin = code[0].replace(/-/gi, "");
-      codeGroup.push(this.pinFormat(pin));
+      codeGroup.push(this._pinFormat(pin));
     }
     return codeGroup;
   }
-  pinFormat(pin) {
+  _pinFormat(pin) {
     return [pin.slice(0, 4), pin.slice(4, 8), pin.slice(8, 12), pin.slice(12)];
   }
 }
